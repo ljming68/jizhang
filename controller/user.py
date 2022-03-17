08268b5659a05d0a1e4e7568c03de33e51573574
LJMing <376855074@qq.com>
2022-03-17 20:52:44 +0800

    完成登录注册接口

--- controller/user.py
+++ controller/user.py
@@ -1,32 +1,16 @@
 from flask import Blueprint,request,session,make_response
 from model.users import Users
 from common.makecode import ImageCode,EmailCode
-import re
+import re, hashlib
 
 
 user = Blueprint('user',__name__)
 
-@user.route('/login',methods=['POST'])
-# index index_show 不可以写成index 
-def login():
-    user = Users()
-    username = request.form.get('username').strip()
-    password = request.form.get('password').strip()
-
-    result = user.find_by_username(username)
-    if len(result) == 1 and result[0].password ==password:
-        session['islogin'] = 'true'
-        session['userid'] = result[0].userid
-        session['username'] = username
-        session['nickname'] = result[0].nickname
-        session['roleid'] = result[0].roleid
-        return 'login-success'
-    # print(result)
-    # return '11'
 
 @user.route('/vcode')
 def vcode():
     code,bstring = ImageCode().get_code()
+    print(code)
     response = make_response(bstring)
     response.headers['Content-Type'] = 'image/jpeg'
     session['vcode'] = code.lower()
@@ -39,6 +23,7 @@ def ecode():
         return 'email-invalid'
     
     code = EmailCode().gen_email_code()
+    # print(code)
     try:
         EmailCode().send_email(email,code)
         # 将邮箱验证码保存在Session中
@@ -46,3 +31,63 @@ def ecode():
         return 'send-success'
     except:
         return 'send-fail'
+
+@user.route('/login',methods=['POST'])
+# index index_show 不可以写成index 
+def login():
+    user = Users()
+    username = request.form.get('username').strip()
+    password = request.form.get('password').strip()
+    vcode = request.form.get('vcode').lower().strip()
+
+    # 校验图形验证码是否正确 
+    # ！！！0000 在正式上线时要删掉
+    if vcode != session.get('vcode') and vcode != '0000':
+        return 'vcode-error'
+    
+    else:
+        #实现登录功能
+        password = hashlib.md5(password.encode()).hexdigest()
+        result = user.find_by_username(username)
+        if len(result) == 1 and result[0].password ==password:
+            session['islogin'] = 'true'
+            session['userid'] = result[0].userid
+            session['username'] = username
+            session['nickname'] = result[0].nickname
+            session['roleid'] = result[0].roleid
+            return 'login-success'
+        
+
+@user.route('/register',methods=['POST'])
+def register():
+    user = Users()
+    username = request.form.get('username').strip()
+    password = request.form.get('password').strip()
+    ecode = request.form.get('ecode').strip()
+
+    # 校验邮箱验证码是否正确
+    if ecode != session.get('ecode'):
+        return 'ecode-error'
+    
+    # 验证邮箱地址的正确性
+    elif not re.match('.+@.+\..+', username):
+        return 'email-invalid'
+    
+    # 验证密码的有效性
+    elif len(password) < 5:
+        return 'password-invalid'
+    
+    # 验证用户是否已经注册
+    elif len(user.find_by_username(username)) > 0:
+        return 'user-repeated'
+    
+    else:
+        # 实现注册功能
+        password = hashlib.md5(password.encode()).hexdigest()
+        result = user.do_register(username,password)
+        session['islogin'] = 'true'
+        session['userid'] = result.userid
+        session['username'] = username
+        session['nickname'] = result.nickname
+        session['roleid'] = result.roleid
+        return 'register-success'
--- model/users.py
+++ model/users.py
@@ -1,12 +1,26 @@
 from sqlalchemy import Table
 from common.database import dbconnect
+import time,random
 
 dbsession, md, DBase = dbconnect()
 
 class Users(DBase):
     __table__ = Table('users',md,autoload=True)
 
-    # 查询用户
+    # 查询用户名，可用于注册时判断用户名是否已注册，也可用于登录校验
     def find_by_username(self,username):
         result = dbsession.query(Users).filter_by(username=username).all()  
         return result
+    
+    # 实现注册，首次注册时用户只需要输入用户名和密码，所以只需要两个参数
+    # 注册时，在模型类中为其他字段尽力生成一些可用的值，虽不全面，但可用
+    # 通常用户注册时不建议填写太多资料，影响体验，可待用户后续逐步完善
+    def do_register(self,username,password):
+        now = time.strftime('%Y-%m-%d %H:%M:%S')
+        nickname = username.split('@')[0]  # 默认将邮箱账号前缀作为昵称
+        avatar = str(random.randint(1, 15))  # 从15张头像图片中随机选择一张
+        user = Users(username=username, password=password, roleid=2,
+                    nickname=nickname, avatar=avatar + '.png', createtime=now, updatetime=now)
+        dbsession.add(user)
+        dbsession.commit()
+        return user
